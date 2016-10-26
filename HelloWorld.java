f65c2b2b23b4ca0840b3dbadf37a96dd0066b9aa
Hussein Mohammed <zidhussein@gmail.com>
2016-10-26 14:37:26 +0100

    Two loops

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        for (int i = 0; i < 7; i++) {
+        for (int i = 0; i < 9; i++) {
+            System.out.println("Hello World");
+        }
+        for (int i = 0; i < 9; i++) {
             System.out.println("Hello World");
         }
     }
