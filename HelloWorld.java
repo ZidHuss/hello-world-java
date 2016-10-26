0bc4f3652088f8b871139c867817d89e07b8b1c2
Hussein Mohammed <zidhussein@gmail.com>
2016-10-26 14:44:36 +0100

    Added Six

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        int six = 6;
         for (int i = 0; i < 9; i++) {
             System.out.println("Hello World");
         }
